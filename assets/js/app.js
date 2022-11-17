dbbdd8829919cd4193641e2497b094e85a4001b6
HamzaBlala49 <hamzaob7@gmail.com>
2022-11-17 17:51:53 +0300

    ovrflow fixed

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Selectors
+
+// img1 =document.getElementById("")
+// txt1 =document.getElementById("")
+// console.log(img1)
+// console.log(txt1)
+
+// if (screen.width > 500){
+// }else{
+// }
